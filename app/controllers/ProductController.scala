d5a6c4f188773ed152b1efc913aa5759e4e42281
alex alarcon <alexalarcon364@gmail.com>
2016-05-09 14:18:00 -0400

    add new product impl

--- app/controllers/ProductController.scala
+++ app/controllers/ProductController.scala
@@ -1,9 +1,13 @@
 package controllers
 
 import com.google.inject.Inject
-import dao.{ProductsDao}
+import dao.ProductsDao
+import models.{Product, User}
+import play.api.data.Forms._
+import play.api.data.{Form, Forms}
 import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
+import play.api.data.format.Formats._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -13,9 +17,35 @@ import scala.concurrent.Future
   */
 class ProductController @Inject() (productDao: ProductsDao) extends Controller {
 
+  val productForm: Form[Product] = Form {
+    mapping(
+      "id" -> ignored(0L),
+      "name" -> text,
+      "brand" -> text,
+      "price" -> of(doubleFormat),
+      "size" -> text,
+      "kind" -> text,
+      "quantity" -> longNumber,
+      "percentage" -> of(doubleFormat),
+      "origin" -> text,
+      "status" -> text
+    )(Product.apply)(Product.unapply)
+  }
+
+  def newProduct = Action { implicit request =>
+    Ok(views.html.new_product())
+  }
+
   def getProducts = Action.async { implicit request =>
     productDao.findAll().map { product =>
       Ok(Json.toJson(product))
     }
+  }
+
+  def addProduct = Action.async { implicit  request =>
+    // Bind the form first, then fold the result, passing a function to handle errors, and a function to handle success.
+    val prod = productForm.bindFromRequest().get
+    productDao.insert(prod.name, prod.brand, prod.price, prod.size, prod.kind, prod.quantity, prod.percentage, prod.origin, prod.status)
+    Future(Redirect(routes.ProductController.newProduct()).flashing("success" -> "Product was added"))
   }
 }
--- app/dao/ProductsDao.scala
+++ app/dao/ProductsDao.scala
@@ -1,9 +1,11 @@
 package dao
 
 import com.google.inject.Inject
-import models.Product
+import models.{Product, User}
 import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
 import slick.driver.JdbcProfile
+
+import scala.concurrent.Future
 
 /**
   * Created by alex_alarcon on 5/8/2016.
@@ -33,6 +35,18 @@ class ProductsDao @Inject()(protected val dbConfigProvider: DatabaseConfigProvid
       p <- products
     } yield p
     db.run(q.sortBy(p => p.id.asc).result)
+  }
+
+  def insert(name: String, brand: String, price: Double, size: String, kind: String, quantity: Long, percentage: Double, origin: String, status: String ): Future[Product] = db.run {
+    // We create a projection of just the name and age columns, since we're not inserting a value for the id column
+    (products.map(p => (p.name, p.brand, p.price, p.size, p.kind, p.quantity, p.percentage, p.origin, p.status))
+      // Now define it to return the id, because we want to know what id was generated for the person
+      returning products.map(_.id)
+      // And we define a transformation for the returned value, which combines our original parameters with the
+      // returned id
+      into ((pInfo, id) => Product(id, pInfo._1, pInfo._2, pInfo._3, pInfo._4, pInfo._5, pInfo._6, pInfo._7, pInfo._8, pInfo._9))
+      // And finally, insert the person into the database
+      ) += (name, brand, price, size, kind, quantity, percentage, origin, status)
   }
 
 }
