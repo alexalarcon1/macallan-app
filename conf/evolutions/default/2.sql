3fa584dee656deab2545b10ddbb32c18f0bb9cee
alex alarcon <alexalarcon364@gmail.com>
2016-05-16 23:03:58 -0400

    disabled evolutions

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -1,4 +1,3 @@
--- noinspection SqlNoDataSourceInspectionForFile
 # --- !Ups
 INSERT INTO users (username, password, first_name, last_name, address, phone_number, role) VALUES ('warriorsPG', '1234', 'Steph', 'Curry', '123 Golden St.', '+151612345678', 'manager');
 /*
