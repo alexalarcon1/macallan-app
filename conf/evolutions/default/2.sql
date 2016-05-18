ee9d7755adb61e836ee2adc55aae8087e2365790
alex alarcon <alexalarcon364@gmail.com>
2016-05-17 22:27:08 -0400

    changed conf and proc

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -1,5 +1,5 @@
 # --- !Ups
-INSERT INTO users (username, password, first_name, last_name, address, phone_number, role) VALUES ('warriorsPG', '1234', 'Steph', 'Curry', '123 Golden St.', '+151612345678', 'manager');
+INSERT INTO users (username, password, first_name, last_name, address, phone_number, role) VALUES ('admin', 'admin1234', 'Admin', 'User', '123 Golden St.', '+151612345678', 'manager');
 
 # --- !Downs
 DELETE FROM users;
