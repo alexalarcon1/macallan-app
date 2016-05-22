7186b9bc7b1d13342584bc6e0f174f94c0d46a1c
alex alarcon <alexalarcon364@gmail.com>
2016-05-21 22:58:43 -0400

    new order added to db table

--- app/controllers/OrdersController.scala
+++ app/controllers/OrdersController.scala
@@ -1,8 +1,9 @@
 package controllers
 
 import com.google.inject.Inject
-import dao.OrdersDao
+import dao.{NewOrdersDao, OrdersDao}
 import models.{NewOrder, Order}
+import play.api.Logger
 import play.api.data.Form
 import play.api.data.Forms._
 import play.api.data.format.Formats._
@@ -15,7 +16,7 @@ import scala.concurrent.Future
 /**
   * Created by alex_alarcon on 5/8/2016.
   */
-class OrdersController @Inject()(ordersDao: OrdersDao) extends Controller {
+class OrdersController @Inject()(newOrdersDao: NewOrdersDao) extends Controller {
 
  /* def getOrders = Action.async { implicit request =>
     ordersDao.findByStatus("pendng").map { order =>
@@ -23,13 +24,39 @@ class OrdersController @Inject()(ordersDao: OrdersDao) extends Controller {
     }
   }*/
 
+  def newOrder = Action { implicit request =>
+    Ok(views.html.new_order())
+  }
+
+  def viewCurrentOrders = Action { implicit request =>
+    Ok(views.html.current_orders())
+  }
+
+  def viewOrderHistory = Action { implicit request =>
+    Ok(views.html.order_history())
+  }
+
   //TODO bind OrderForm to create new orders
   val newOrderForm: Form[NewOrder] = Form {
     mapping(
-      "product_name" -> text,
-      "product_brand" -> text,
+      "id" -> ignored(0L),
+      "product_name" -> nonEmptyText(2,15),
+      "product_brand" -> nonEmptyText(2,15),
       "product_size" -> of(doubleFormat),
       "product_quantity" -> longNumber
     )(NewOrder.apply)(NewOrder.unapply)
+  }
+
+  def addNewOrder = Action.async { implicit  request =>
+    // Bind the form first, then fold the result, passing a function to handle errors, and a function to handle success.
+    val ord = newOrderForm.bindFromRequest().get
+
+    Logger.info(ord.product_name)
+    Logger.info(ord.product_brand)
+    Logger.info(s"${ord.product_size}")
+    Logger.info(s"${ord.product_quantity}")
+
+    newOrdersDao.insert(ord.product_name, ord.product_brand, ord.product_size, ord.product_quantity)
+    Future(Redirect(routes.OrdersController.viewCurrentOrders()))
   }
 }
---
+++ app/models/NewOrder.scala
@@ -0,0 +1,46 @@
+package models
+
+import play.api.libs.json.{JsNumber, JsString, Json, Writes}
+
+/**
+  * Created by alex_alarcon on 5/21/2016.
+  */
+
+case class  NewOrder(id: Long=0,
+                     product_name: String,
+                     product_brand: String,
+                     product_size: Double,
+                     product_quantity: Long) {
+  def toJson = {
+    Json.obj("id" -> JsNumber(id),
+      "product_name" -> JsString(product_name),
+      "product_brand" -> JsString(product_brand),
+      "product_size" -> JsNumber(product_size),
+      "product_quantity" -> JsNumber(product_quantity)
+    )
+  }
+}
+
+object NewOrder {
+  implicit val newOrderWrites = new Writes[NewOrder] {
+    def writes(nOrd: NewOrder) = Json.obj(
+      "id" -> JsNumber(nOrd.id),
+      "name" -> JsString(nOrd.product_name),
+      "brand" -> JsString(nOrd.product_brand),
+      "size" -> JsNumber(nOrd.product_size),
+      "quantity" -> JsNumber(nOrd.product_quantity)
+    )
+  }
+
+}
+//products: List[NewProducts])
+/*object NewOrder {
+  implicit val reads: Reads[NewOrder] = (
+      (JsPath \ "order" \ "product_name").read[String] and
+      (JsPath \ "order" \ "product_brand").read[String] and
+      (JsPath \ "order" \ "product_size").read[Double] and
+      (JsPath \ "order" \ "product_quantity").read[Long]
+      //(JsPath \ "order" \ "products").read[List[NewProducts]]
+    )(NewOrder.apply _)
+
+}*/
